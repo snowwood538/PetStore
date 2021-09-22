0d61ff1f0a6787bd0be5ae970f6478a3db93c666
Evgenii Posukhov <Evgenii Posukhov@epam.com>
2021-09-23 02:17:18 +0400

    -File management

--- PetStoreRestTesting/src/main/java/swagger/petstore/api/instances/urls/BaseUrls.java
+++ PetStoreRestTesting/src/main/java/swagger/petstore/api/instances/urls/BaseUrls.java
@@ -1,4 +1,4 @@
-package swagger.petstore.api_instances;
+package swagger.petstore.api.instances.urls;
 
 public class BaseUrls {
     public static final String BASE_URL = "https://petstore.swagger.io/v2";
