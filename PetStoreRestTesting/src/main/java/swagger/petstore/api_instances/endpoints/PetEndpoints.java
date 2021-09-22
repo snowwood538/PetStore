7e51c88ef512b7d3a9ab9156937e2e83fc4631f9
Evgenii Posukhov <Evgenii Posukhov@epam.com>
2021-09-23 02:07:34 +0400

    -File management

--- PetStoreRestTesting/src/main/java/swagger/petstore/api_instances/endpoints/PetEndpoints.java
+++
@@ -1,6 +0,0 @@
-package swagger.petstore.api_instances.endpoints;
-
-public class PetEndpoints {
-    public static final String GET_PETS_BY_STATUS = "/pet/findByStatus/";
-    public static final String GET_PETS_BY_ID = "/pet/";
-}
--- PetStoreRestTesting/src/main/java/swagger/petstore/api_instances/queries/PetQueries.java
+++
@@ -1,5 +0,0 @@
-package swagger.petstore.api_instances.queries;
-
-public class PetQueries {
-    public static final String petStatus = "status";
-}
