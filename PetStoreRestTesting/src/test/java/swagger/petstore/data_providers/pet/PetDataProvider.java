dcd7e09caf3cbb4dc6500e3c271afae1b7fda97d
Evgenii Posukhov <Evgenii Posukhov@epam.com>
2021-09-24 17:13:13 +0400

    -Test commit

--- PetStoreRestTesting/src/test/java/swagger/petstore/data_providers/pet/PetDataProvider.java
+++ PetStoreRestTesting/src/test/java/swagger/petstore/data_providers/pet/PetDataProvider.java
@@ -2,10 +2,6 @@ package swagger.petstore.data_providers.pet;
 
 import org.testng.annotations.DataProvider;
 
-import static swagger.petstore.api.instances.file_paths.Paths.DOC_FILE_PATH;
-import static swagger.petstore.api.instances.file_paths.Paths.HTML_FILE_PATH;
-import static swagger.petstore.api.instances.file_paths.Paths.TEXT_FILE_PATH;
-
 public class PetDataProvider {
 
     @DataProvider(name = "Correct_pet_statuses")
@@ -34,13 +30,5 @@ public class PetDataProvider {
                 {new String[] {"qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq"}},
                 {new String[] {"DROP TABLE"}},
                 {new String[] {"https://yandex.ru/"}}};
-    }
-
-    @DataProvider(name = "Path_to_files_to_add_images")
-    public static Object[][] getPathsToFiles() {
-        return new Object[][]{
-                {TEXT_FILE_PATH},
-                {DOC_FILE_PATH},
-                {HTML_FILE_PATH}};
     }
 }
